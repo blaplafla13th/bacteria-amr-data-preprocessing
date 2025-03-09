9d66fa20f1371adc5756aa3841fb655e24e4b480
blaplafla13th <phambathanghp@gmail.com>
2025-03-10 01:18:06 +0700

    add run in background for lazy script

--- scripts/all_in_one.sh
+++ scripts/all_in_one.sh
@@ -1,6 +1,9 @@
-bwa-mem2 index sequence.fasta
+bwa-mem2 index sequence.fasta &
 python ../scripts/filter_data.py
 python ../scripts/select_data.py
-bash ../scripts/ena-downloader.sh
+bash ../scripts/ena-downloader.sh &
+watch_process=$!
+sleep 100
+while ps -p $watch_process > /dev/null; do nextflow ../scripts/VariantCalling.nf -resume; sleep 100; done
 nextflow ../scripts/VariantCalling.nf -resume
 python ../scripts/final.py
